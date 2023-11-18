260ef56844f550eaa665c6cb18ec59fab3156d98
Andre <64148828+Andre-Codes@users.noreply.github.com>
2023-11-18 10:11:06 -0600

    updated front page text

--- Home.py
+++ Home.py
@@ -13,5 +13,5 @@ st.markdown("""
 Snap a photo of text from a book, magazine, comic, or anything with words!
 Word Canvas will turn your imagery into beautiful artwork.
 
-See what you read, enjoy what you imagine.
+Image what you read, see what you imagine.
 """)
