0584fed8d01f488257a3c801301b0b4ac165e184
Andre <64148828+Andre-Codes@users.noreply.github.com>
2023-11-25 17:10:16 -0600

    updated page info

--- Home.py
+++ Home.py
@@ -15,3 +15,23 @@ Word Canvas will turn your imagery into beautiful artwork.
 
 Image what you read, see what you imagine.
 """)
+
+col1, col2, col3 = st.columns([1, 3, 1])
+
+with col2:
+    st.markdown("""
+👈Enter the imagery room from the sidebar.
+    """)
+
+st.warning("Refreshing the webpage will clear all image history", icon='ℹ️')
+
+disclaimer = st.sidebar.container()
+disclaimer.info("""
+The content provided on this platform is generated using 
+artificial intelligence (AI) techniques. AI-generated 
+content is subject to potential errors, biases, and inaccuracies inherent 
+to automated systems. Users are advised to exercise discretion and 
+critically evaluate the content before relying on it for decision-making, 
+coding, or any other purpose. Official OpenAI terms and policies can be
+read here: https://openai.com/policies
+""", icon="ℹ️")
